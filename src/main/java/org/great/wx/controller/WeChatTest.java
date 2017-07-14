89801c69bc27629d66d1c2a15c78f5713b037c29
524987246 <524987246@qq.com>
2017-07-14 16:34:12 +0800

    1

--- src/main/java/org/great/wx/controller/WeChatTest.java
+++ src/main/java/org/great/wx/controller/WeChatTest.java
@@ -9,9 +9,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.dom4j.DocumentException;
-import org.great.util.MessageUtil;
-import org.great.util.weixin.WeChatCheckUtil;
 import org.great.wx.bean.TextMessage;
+import org.great.wx.wxutil.MessageUtil;
+import org.great.wx.wxutil.WeChatCheckUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,7 +27,6 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/WeChat/test")
 public class WeChatTest {
 
-
 	/**
 	 * 微信端测试连接
 	 * 
@@ -35,8 +34,7 @@ public class WeChatTest {
 	 * @throws IOException
 	 */
 	@RequestMapping(value = "/connect.html", method = RequestMethod.GET)
-	public void connect(HttpServletResponse response, HttpServletRequest request)
-			throws IOException {
+	public void connect(HttpServletResponse response, HttpServletRequest request) throws IOException {
 		System.out.println("微信服务测试开始");
 		String signature = request.getParameter("signature");
 		String timestamp = request.getParameter("timestamp");
@@ -55,8 +53,7 @@ public class WeChatTest {
 	 * @throws IOException
 	 */
 	@RequestMapping(value = "/connect.html", method = RequestMethod.POST)
-	public void receive(HttpServletResponse response, HttpServletRequest request)
-			throws IOException {
+	public void receive(HttpServletResponse response, HttpServletRequest request) throws IOException {
 		response.setCharacterEncoding("UTF-8");
 		request.setCharacterEncoding("UTF-8");
 		PrintWriter out = response.getWriter();
@@ -74,15 +71,15 @@ public class WeChatTest {
 				textMessage.setMsgType("text");
 				textMessage.setCreateTime(new Date().getTime());
 				textMessage.setContent("接收成功==>" + content + "<==,返回消息");
-				message=MessageUtil.initText(toUserName, fromUserName, content);
-			}else if(MessageUtil.MSGTYPE_EVENT.equals(msgType)){
-				//事件类型再去细化判断
-				String eventType=map.get("Event");
-				if(MessageUtil.EVENT_SUBSCRIBE.equals(eventType)){
-					message=MessageUtil.initText(toUserName, fromUserName, "");
+				message = MessageUtil.initText(toUserName, fromUserName, content);
+			} else if (MessageUtil.MSGTYPE_EVENT.equals(msgType)) {
+				// 事件类型再去细化判断
+				String eventType = map.get("Event");
+				if (MessageUtil.EVENT_SUBSCRIBE.equals(eventType)) {
+					message = MessageUtil.initText(toUserName, fromUserName, "");
 				}
-			}else{
-				message=MessageUtil.initText(toUserName, fromUserName, "");
+			} else {
+				message = MessageUtil.initText(toUserName, fromUserName, "");
 			}
 			out.print(message);
 		} catch (DocumentException e) {
