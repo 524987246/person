14f9c4e7232f4308f76e8b1a580353e3f0dc2f4c
524987246 <524987246@qq.com>
2017-08-24 16:16:52 +0800

    工作时长计算,未完善,待完善

--- src/main/java/org/great/util/DateUtil.java
+++ src/main/java/org/great/util/DateUtil.java
@@ -1,6 +1,7 @@
 package org.great.util;
 
 import java.text.SimpleDateFormat;
+import java.text.ParseException;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -11,31 +12,43 @@ public class DateUtil {
 	public final static String DATE_PATTERN = "yyyy-MM-dd";
 	/** 时间格式(yyyy-MM-dd HH:mm:ss) */
 	public final static String DATE_TIME_PATTERN = "yyyy-MM-dd HH:mm:ss";
-	
-	public static String format(Date date) {
-        return format(date, DATE_PATTERN);
-    }
 
-    public static String format(Date date, String pattern) {
-        if(date != null){
-            SimpleDateFormat df = new SimpleDateFormat(pattern);
-            return df.format(date);
-        }
-        return null;
-    }
-    
-    /**
-     * 时间计算
-     * @param olddate
-     * @param newdate
-     * @return olddate与newdate 时间相差天数
-     */
-    public static long compareDate(Date olddate, Date newdate) {
-		if (newdate == null) {
-			newdate = new Date();
+	public static String format(Date date) {
+		return format(date, DATE_PATTERN);
+	}
+
+	public static String format(Date date, String pattern) {
+		if (date != null) {
+			SimpleDateFormat df = new SimpleDateFormat(pattern);
+			return df.format(date);
 		}
-		long diff = olddate.getTime() - newdate.getTime();
-		long days = diff / (1000 * 60 * 60 * 24);
+		return null;
+	}
+
+	/**
+	 * 计算两个时间差
+	 * 
+	 * @param enddate
+	 *            结束时间
+	 * @param begindate
+	 *            开始时间
+	 * @param flag
+	 *            1 精确到小时 2精确到天 3精确到分
+	 * @return
+	 */
+	public static long compareDate(Date enddate, Date begindate, int flag) {
+		if (begindate == null) {
+			begindate = new Date();
+		}
+		long diff = enddate.getTime() - begindate.getTime();
+		long days = 0;
+		if (flag == 2) {// 天
+			days = diff / (1000 * 60 * 60 * 24);
+		} else if (flag == 1) {// 时
+			days = diff / (1000 * 60 * 60);
+		} else if (flag == 3) {// 分
+			days = diff / (1000 * 60);
+		}
 		return days;
 	}
 
@@ -63,13 +76,13 @@ public class DateUtil {
 
 	/**
 	 * 设置基本查询,时间限制一个月
+	 * 
 	 * @param begintime
 	 * @param endtime
 	 * @param map
 	 * @return
 	 */
-	public static Map<String, Object> setQueryDate(String begintime,
-			String endtime, Map<String, Object> map) {
+	public static Map<String, Object> setQueryDate(String begintime, String endtime, Map<String, Object> map) {
 		Boolean bo1 = (begintime == null || begintime.trim().equals(""));
 		Boolean bo2 = (endtime == null || endtime.trim().equals(""));
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
@@ -78,7 +91,7 @@ public class DateUtil {
 			endtime = format(new Date(), "yyyy-MM-dd");
 			begintime = getDate(null, -30);
 		} else if (bo1 & !bo2) {
-			//结束时间不为空
+			// 结束时间不为空
 			try {
 				date = sdf.parse(endtime);
 			} catch (Exception e) {
@@ -93,11 +106,74 @@ public class DateUtil {
 			}
 			endtime = getDate(date, 30);
 		}
-		if(map==null){
-			map=new HashMap<String, Object>();
+		if (map == null) {
+			map = new HashMap<String, Object>();
 		}
 		map.put("begintime", begintime);
 		map.put("endtime", endtime);
 		return map;
+	}
+
+	/**
+	 * 获取总共请假时长
+	 * 
+	 * @param begindate
+	 * @param enddate
+	 * @throws ParseException
+	 */
+	public static void getworkhours(String begindate, String enddate) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		Date begindate_date = sdf.parse(begindate);
+		Date enddate_date = sdf.parse(enddate);
+		long days = 0;
+		days += getworkhours(begindate);
+		days += getworkhours(enddate);
+		days += (compareDate(enddate_date, begindate_date, 2) - 1) * 8 * 60;
+		System.out.println(days);
+	}
+
+	/**
+	 * 获取当天多少小时
+	 * 
+	 * @param dateStr
+	 * @return
+	 * @throws ParseException
+	 */
+	public static long getworkhours(String dateStr) throws ParseException {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String morning_end = dateStr.substring(0, 9) + " 12:00:00";
+		String morning_begin = dateStr.substring(0, 9) + " 08:30:00";
+		String afternoon_end = dateStr.substring(0, 9) + " 17:30:00";
+		String afternoon_begin = dateStr.substring(0, 9) + " 13:00:00";
+		Date morning_end_date = sdf.parse(morning_end);
+		Date morning_begin_date = sdf.parse(morning_begin);
+		Date afternoon_end_date = sdf.parse(afternoon_end);
+		Date afternoon_begin_date = sdf.parse(afternoon_begin);
+		Date date = sdf.parse(dateStr);
+		long days = 0;
+		if (date.compareTo(morning_end_date) < 0 && date.compareTo(morning_begin_date) > 0) {// 现在时间比早上时间大
+			days = compareDate(date, morning_begin_date, 3);
+		} else if (date.compareTo(morning_end_date) >= 0) {
+			days += 210;
+		}
+		if (date.compareTo(afternoon_end_date) < 0 && date.compareTo(afternoon_begin_date) > 0) {
+			days += compareDate(date, afternoon_begin_date, 3);
+		} else if (date.compareTo(afternoon_end_date) >= 0) {
+			days += 270;
+		}
+		// System.out.println(days);
+		return days;
+	}
+
+	public static void main(String[] args) throws ParseException {
+		String begin = "2017-8-24 08:30:00";
+		String end = "2017-8-25 17:30:00";
+		getworkhours(begin, end);
+		// getworkhours(begin, end);
+		// SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		// Date begindate = sdf.parse(begin);
+		// Date enddate = sdf.parse(end);
+		// long days = compareDate(enddate, begindate, 1);
+		// System.out.println(days);
 	}
 }
--- src/main/java/org/great/util/Dboperate.java
+++ src/main/java/org/great/util/Dboperate.java
@@ -5,21 +5,22 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Dboperate {
-	public static Connection getConnection(String driver, String url,
-			String username, String userpwd) {
+	public static Connection getConnection(String driver, String url, String username, String userpwd) {
 		Connection con = null;
 		try {
 			Class.forName(driver);
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
 			return null;
 		}
 		try {
 			con = DriverManager.getConnection(url, username, userpwd);
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
 			return null;
 		}
 		return con;
@@ -27,32 +28,97 @@ public class Dboperate {
 
 	public static void main(String[] args) throws SQLException {
 		String driver = "com.mysql.jdbc.Driver";
-		String url = "jdbc:mysql://localhost:3306/db_person?useUnicode=true&amp;characterEncoding=UTF-8";
+		String url = "jdbc:mysql://localhost:3306/kaisioa?useUnicode=true&amp;characterEncoding=UTF-8";
 		String username = "root";
 		String userpwd = "JF1602";
+
+
 		Connection con = getConnection(driver, url, username, userpwd);
-//		System.out.println(con);
-		String sql = "SELECT * FROM tb_costinfo WHERE income_expend=1 AND createtime>='2017-03-01'";
+		// Connection con2 = getConnection(driver2, url2, username2, userpwd2);
+		// System.out.println(con);
+		String sql = "select id,del_flag,parent_id,surrogate,name from buz_company";
 		PreparedStatement stmt = con.prepareStatement(sql);
 		ResultSet set = stmt.executeQuery();
+		List<Testbean> list = new ArrayList<Testbean>();
+
 		while (true) {
 			if (set.next()) {
-				System.out.print(set.getString("createtime")+"==");
-				System.out.print(set.getString("endtime")+"==");
-				System.out.print(set.getString("payname")+"==");
-				System.out.print(set.getString("paymoney")+"==");
-				System.out.print(set.getString("income_expend"));
-				System.out.println();
+				Testbean testbean = new Testbean();
+				String sbf = "";
+				String str = null;
+				str = set.getString("id");
+				testbean.id = str;
+				str = set.getString("name");
+				testbean.name = str;
+//				str = set.getString("surrogate");
+//				testbean.surrogate = str;
+//				str = set.getString("del_flag");
+//				if (str.equals("0")) {
+//					testbean.del_flag = "1";
+//				} else {
+//					testbean.del_flag = "0";
+//				}
+//				str = set.getString("parent_id");
+//				testbean.parent_id=str;
+//				System.out.print(testbean.id);
+//				if(str==null||str.trim().equals("")){
+//					System.exit(-1);
+//				}
+				while (!str.equals(testbean.id)) {
+					String flag = str;
+					sbf = str + "," + sbf;
+					str = getparent(sql, str, con);
+					if (str == null) {
+						testbean.del_flag = "1";
+						System.out.println("name="+testbean.name);
+						list.add(testbean);
+						break;
+					}else if(str.equals(flag)){
+						break;
+					}
+				}
+//				if(!testbean.surrogate.equals("0")){
+//					sbf = sbf+testbean.surrogate+",";
+//					testbean.parent_id=testbean.surrogate;
+//				}
+				// sbf = str + "," + sbf;
+//				sbf = sbf.trim();
+//				if(sbf.equals("")){
+//					sbf = str + "," + sbf;
+//				}
+//				testbean.parent_ids = sbf;
+//				System.out.println("===" + sbf+"<parent_id="+testbean.parent_id+">");
+				
 			} else {
 				break;
 			}
 		}
-		try {
-			con.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		System.out.println("要更新的集合长度" + list.size());
+//		sql = "UPDATE buz_company SET del_flag='A1' WHERE id=A3";
+//		try {
+//			stmt.close();
+//			if (set != null) {
+//				set.close();
+//			}
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//		}
+//		PreparedStatement stmt2 = con.prepareStatement(sql);
+//		int i = 1;
+//		for (Testbean temp : list) {
+//			String flag = sql.replaceAll("A1", temp.del_flag);
+//			//flag = flag.replaceAll("A2", temp.parent_ids);
+//			flag = flag.replaceAll("A3", temp.id);
+//			System.out.println(i + "==" + flag);
+//			i++;
+//			int j = stmt2.executeUpdate(flag);
+//			if (j > 0) {
+//				System.out.println(true);
+//			} else {
+//				System.out.println(false);
+//			}
+//		}
+
 		// driver="oracle.jdbc.driver.OracleDriver";
 		// url="jdbc:oracle:thin:@127.0.0.1:1521:ORCL";
 		// username="JF1602";
@@ -60,4 +126,29 @@ public class Dboperate {
 		// Connection con1=getConnection(driver,url,username,userpwd);
 		// System.out.println(con1);
 	}
+
+	private static String getparent(String sql, String id, Connection con) throws SQLException {
+		sql = sql + " where id='" + id+"'";
+		PreparedStatement stmt = con.prepareStatement(sql);
+		ResultSet set = stmt.executeQuery();
+		while (true) {
+			if (set.next()) {
+				sql = set.getString("parent_id");
+				return sql;
+			} else {
+				break;
+			}
+		}
+		return null;
+	}
+}
+
+class Testbean {
+	public String id;
+	public String del_flag;
+	public String surrogate;
+	public String parent_id;
+	public String parent_ids;
+	public String name;
+
 }
