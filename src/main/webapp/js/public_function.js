2ce90417aba1af446b7af9445305a43a0a531d7e
524987246 <524987246@qq.com>
2018-01-22 13:52:00 +0800

    12

--- src/main/webapp/js/public_function.js
+++ src/main/webapp/js/public_function.js
@@ -5,6 +5,48 @@ function dir(obj) {
 	}
 	console.dir(obj);
 }
+//判断浏览器类型
+function myBrowser() {
+	var userAgent = navigator.userAgent; //取得浏览器的userAgent字符串
+	if (userAgent.indexOf("Opera") > -1) {
+		return "Opera";
+	}
+	//判断是否Opera浏览器
+	if (userAgent.indexOf("Firefox") > -1) {
+		return "FF";
+	}
+	//判断是否Firefox浏览器
+	if (userAgent.indexOf("Chrome") > -1) {
+		return "Chrome";
+	}
+	;
+	if (userAgent.indexOf("Safari") > -1) {
+		return "Safari";
+	}
+	;
+	//判断是否Safari浏览器
+	if (userAgent.indexOf("compatible") > -1 && userAgent.indexOf("MSIE") > -1 && !isOpera) {
+		return "IE";
+	}
+	; //判断是否IE浏览器
+}
+function isJSON(str) {
+	if (typeof str == 'string') {
+		try {
+			var obj = JSON.parse(str);
+			if (typeof obj == 'object' && obj) {
+				return true;
+			} else {
+				return false;
+			}
+
+		} catch (e) {
+			console.log('error：' + str + '!!!' + e);
+			return false;
+		}
+	}
+	console.log('It is not a string!')
+}
 /**
  * 项目项目根路径
  * http://127.0.0.1:8080/.....
